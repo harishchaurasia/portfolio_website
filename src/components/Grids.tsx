628e7d7f1dde4a7b4f572884390cdda2ad1ab2ee
Harish <hchaura1@asu.edu>
2024-07-24 08:35:27 +0530

    bck again

--- src/components/Grids.tsx
+++ src/components/Grids.tsx
@@ -1,20 +1,24 @@
 // src/components/Grids.tsx
 import React from 'react';
-import ImageWithRoundedCorners from './RoundImages';
 
 const Grids: React.FC = () => {
   return (
-    <div className="grid-background">
-      <div className="block">
-        <ImageWithRoundedCorners 
-          src="/harish_headshot_black.png" 
-          alt="My Image" 
-          width="315px" 
-          height="315px" 
-        />
-        <h1 className="font-imperfect text-white text-4xl mt-4">
-          Hello there...
-        </h1>
+    <div className="grid-background flex flex-col items-center h-screen text-center">
+      
+      <h1 className="font-aloe text-white text-9xl font-bold text-gradient mt-36">Hi. I'm Harish.</h1>
+      <h2 className="font-aloe text-white text-9xl font-bold text-gradient">A Developer.</h2>
+      <p className="font-bruce mt-16 text-2xl text-gray-200">I love building shit. 🛠️⚙️</p>
+      <p className="font-bruce mt-2 text-2xl text-gray-200">Eat. Sleep. Build & Repeat.♾️</p>
+      {/* <RoundedImg 
+        src="/harish_headshot_black.png"
+        alt="My Image"
+        width="315px"
+        height="315px"
+        top="2030px"
+        left="223px"
+      />  */}
+     
+      <div className='relative h-screen mt-36'>
       </div>
     </div>
   );
