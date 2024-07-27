5333bdf773a579e3fdb0f7b0577b93bf74246c07
Harish <harishchaurasia143@gmail.com>
2024-07-27 15:18:04 +0530

    adding border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 // src/app/page.tsx
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import Intro from '../components/Intro';
-import Grids from '../components/Grids';
-import Navs from '../components/Navs';
-import Scene from '../components/Index';
-import About from '../components/About';
+import React, { useState, useEffect } from "react";
+import Intro from "../components/Intro";
+import Grids from "../components/Grids";
+import Navs from "../components/Navs";
+import Scene from "../components/Index";
+import About from "../components/About";
 
 export default function Home() {
   const [showIntro, setShowIntro] = useState(true);
@@ -33,21 +33,26 @@ export default function Home() {
       <section id="home" className="content">
         {!showIntro && showGrid && <Grids />}
       </section>
-      <section id="about" className="content h-screen flex items-center justify-center bg-black text-white">
-        <div className="max-w-max p-8 w-full">
-        <h1 className="text-green-500 text-6xl justify-items-start font-bold mx-36 mb-8">.About Me</h1>
+      <section
+        id="about"
+        className="content h-screen flex items-center justify-center bg-black text-white"
+      >
+        <div className="max-w-max p-8 w-full rounded-lg">
+          <h1 className="text-green-500 text-6xl font-bold text-center md:text-left mx-0 md:mx-36 mb-8">
+            .About Me
+          </h1>
           <About />
         </div>
-    </section>
+      </section>
 
       <section id="projects" className="content">
-      <div className=" text-white max-w-2xl p-8">
-          <h1 className="text-4xl font-bold mb-4">Section 2</h1>
+        <div className=" text-white max-w-2xl p-8">
+          <h1 className="text-4xl font-bold mb-4"></h1>
         </div>
       </section>
       <section id="contact" className="content">
-      <div className="text-white max-w-2xl p-8">
-          <h1 className="text-4xl font-bold mb-4">Section 3</h1>
+        <div className="text-white max-w-2xl p-8">
+          <h1 className="text-4xl font-bold mb-4"></h1>
         </div>
       </section>
     </div>
