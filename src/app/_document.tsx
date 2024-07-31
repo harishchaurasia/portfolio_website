757b1c806603c090679218040f228541cd8a05de
Harish <hchaura1@asu.edu>
2024-07-31 13:01:48 +0530

    vercel/analytics

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,13 +1,13 @@
-// src/app/_document.tsx
+// src/pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <link rel="icon" href="/harish02.png" type="image/png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r122/three.min.js"></script>
+        <link rel="icon" href="/harish02.png" type="image/png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r122/three.min.js"></script>
       </Head>
       <body>
         <Main />
